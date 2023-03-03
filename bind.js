035f712e916dce015a87ac914c130c7e4dd78017
Gary <geehor@gmail.com>
2023-03-03 16:02:32 -0500

    Start myBind version 2

--- bind.js
+++ bind.js
@@ -5,12 +5,20 @@ Function.prototype.myBind = function (ctx) {
   let args = Array.from(arguments);
   let others = args.slice(1);
 
-  const dontKnowHowName =  function () {
-    let args2 = Array.from(arguments).slice(1)
-    that.apply(ctx, others);
-  return dontKnowHowName(args2)
+  return function () {
+    let args2 = Array.from(arguments)
+    return that.apply(ctx, others.concat(args2));
   };
+  // return dontKnowHowName(args2);
 };
+
+Function.prototype.myBindXD = function (ctx, ...args) {
+  let that = this;
+  // args is a real arr
+  return function (...args2) {
+    return that.apply(ctx, args.concat(args2.slice(1)));
+  }
+}
 
 class Cat {
   constructor(name) {
@@ -37,23 +45,23 @@ const pavlov = new Dog("Pavlov");
 // // true
 
 // bind time args are "meow" and "Kush", no call time args
-let bsays = markov.says.myBind(pavlov, "meow", "Kush");
+let bsays = markov.says.myBindXD(pavlov, "meow", "Kush");
 // Pavlov says meow to Kush!
 // true
-bsays()
+bsays();
 // no bind time args (other than context), call time args are "meow" and "a tree"
-markov.says.myBind(pavlov)("meow", "a tree");
+markov.says.myBindXD(pavlov)("meow", "a tree");
 //bsays(args)
 // Pavlov says meow to a tree!
 // true
 
 // bind time arg is "meow", call time arg is "Markov"
-markov.says.myBind(pavlov, "meow")("Markov");
+markov.says.myBindXD(pavlov, "meow")("Markov");
 // Pavlov says meow to Markov!
 // true
 
 // no bind time args (other than context), call time args are "meow" and "me"
-const notMarkovSays = markov.says.myBind(pavlov);
+const notMarkovSays = markov.says.myBindXD(pavlov);
 notMarkovSays("meow", "me");
 // Pavlov says meow to me!
 // true
